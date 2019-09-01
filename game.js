c32f8ebd5fee79753b710a8904d8dfc67a508b36
Glen <ultrapancake@gmail.com>
2019-09-01 16:51:07 -0600

    Push guess to HTML and replace the underscores

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 // Create an array of options
-    var word = ["Spider", "Pig", "Dog", "Cat", "Cow", "Bird", "Horse"];
+    var word = ["spider", "pig", "dog", "cat", "cow", "bird", "horse"];
 // Select Random Word
     var randNum = Math.floor(Math.random() * word.length);
     var randWord = word[randNum];
@@ -8,6 +8,9 @@
 // User's Guess
     var rightWord = [];
     var wrongWord= [];
+
+// HTML
+var docUnderScore = document.getElementsByClassName('rand-word');
 
 // Dynamically post underscores for length of word
 var createUnderScore = () => {
@@ -29,12 +32,19 @@ document.addEventListener('keypress', (event) => {
         // Add to right words array
         rightWord.push(keyWord);
             console.log("right word: " + rightWord);
-        // Replace underscore with correct letter
-        underScore[randWord.indexOf(keyWord)] = keyWord;
+         // Replace underscore with correct letter
+         underScore[randWord.indexOf(keyWord)] = keyWord;
             console.log("Replace array: " + underScore);
+         docUnderScore[0].innerHTML = underScore.join(' ');
         // Win
         if(underScore.join("") === randWord) {
             alert("You Win");
         }
+    }else{
+        wrongWord.push(keyWord);
     }
 })
+
+// Push the under score function to the HTML
+docUnderScore[0].innerHTML = createUnderScore().join(' ');//This function runs twice and produces double underscores
+
